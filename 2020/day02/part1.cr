6fbb67e6c5a8cab368a518f136790cfea745de9b
Anton Maminov <anton.linux@gmail.com>
2020-12-02 13:34:08 +0200

    refactor day 2 part 1

--- 2020/day02/part1.cr
+++ 2020/day02/part1.cr
@@ -1,3 +1,17 @@
+module Comparable(T)
+  # Check if a value between *min* and *max*.
+  #
+  # ```
+  # 3.between?(1, 5)               #=> true
+  # 6.between?(1, 5)               #=> false
+  # "cat".between?("ant", "dog")   #=> true
+  # "gnu".between?("ant", "dog")   #=> false
+  # ```
+  def between?(min, max)
+    self >= min && self <= max
+  end
+end
+
 valid_password_count = 0
 
 File.read_lines("input").each do |line|
@@ -7,7 +21,7 @@ File.read_lines("input").each do |line|
   letter = letter[0]
   count = password.count(letter)
 
-  valid_password_count +=1 if count >= low && count <= high
+  valid_password_count +=1 if count.between?(low, high)
 end
 
-puts valid_password_count
+puts valid_password_count # => 383
