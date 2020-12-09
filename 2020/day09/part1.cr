8539345789025e5aa58c66dbb56f3813ecb8330a
Anton Maminov <anton.linux@gmail.com>
2020-12-09 18:47:28 +0200

    day 9 part 1

---
+++ 2020/day09/part1.cr
@@ -0,0 +1,7 @@
+numbers = File.read_lines("input").map(&.to_i64)
+
+pre = 25
+
+if index = (pre...).find { |i| numbers[i - pre...i].combinations(2).none? { |c| c.sum == numbers[i] } }
+  puts numbers[index]
+end
