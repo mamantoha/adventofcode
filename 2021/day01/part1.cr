89faa31cc4227aa46291d1ca2c278582d03c11d1
Anton Maminov <anton.linux@gmail.com>
2021-12-01 11:59:59 +0200

    use each_cons

--- 2021/day01/part1.cr
+++ 2021/day01/part1.cr
@@ -1,9 +1,4 @@
 numbers = File.read_lines("input").map(&.to_i)
 # numbers = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
 
-result = numbers.each.with_index.reduce(0) do |acc, (x, i)|
-  acc +=1 if x > numbers[i - 1]
-  acc
-end
-
-puts result # => 1766
+puts numbers.each_cons(2).count { |x| x[1] > x[0] }
