2a6d7e4d229431f3fe67734169974ac68427f097
Anton Maminov <anton.linux@gmail.com>
2021-12-02 12:15:11 +0200

    change ruby part1

--- 2021/day02/part01.rb
+++ 2021/day02/part01.rb
@@ -3,16 +3,15 @@ lines = File.readlines("input")
 horizontal_position = depth = 0
 
 lines.each do |line|
-  command, units = line.split
-  units = units.to_i
-
-  case [command, units]
-  in ['forward', Integer]
-    horizontal_position += units
-  in ['up', Integer]
-    depth -= units
-  in ['down', Integer]
-    depth += units
+  if match = /(?<command>forward|down|up) (?<units>\d+)/.match(line)
+    case match.named_captures.transform_keys(&:to_sym)
+    in {command: 'forward', units: units}
+      horizontal_position += units.to_i
+    in {command: 'up', units: units}
+      depth -= units.to_i
+    in {command: 'down', units: units}
+      depth += units.to_i
+    end
   end
 end
 
