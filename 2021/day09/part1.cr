4abf194fa71580bd62d0c5a81eb98d0da7f1dba9
Anton Maminov <anton.linux@gmail.com>
2021-12-09 19:08:27 +0200

    lol

--- 2021/day09/part1.cr
+++ 2021/day09/part1.cr
@@ -9,17 +9,17 @@ heightmap.each_with_index do |locations, y|
   locations.each_with_index do |number, x|
     adjacent_locations = [] of Int32
 
-    left = right = up = down = nil
+    ← = → = ↑ = ↓ = nil
 
-    left = heightmap.dig(y, x - 1) if x > 0
-    right = heightmap.dig(y, x + 1) if x < cols - 1
-    down = heightmap.dig(y - 1, x) if y > 0
-    up = heightmap.dig(y + 1, x) if y < rows - 1
+    ← = heightmap.dig(y, x - 1) if x > 0
+    → = heightmap.dig(y, x + 1) if x < cols - 1
+    ↓ = heightmap.dig(y - 1, x) if y > 0
+    ↑ = heightmap.dig(y + 1, x) if y < rows - 1
 
-    adjacent_locations << left if left
-    adjacent_locations << right if right
-    adjacent_locations << down if down
-    adjacent_locations << up if up
+    adjacent_locations << ← if ←
+    adjacent_locations << → if →
+    adjacent_locations << ↓ if ↓
+    adjacent_locations << ↑ if ↑
 
     low_points << number if adjacent_locations.all? { |n| number < n }
   end
