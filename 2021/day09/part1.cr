250b0861c9097253c00b9dacc68edaaed40ac4ad
Anton Maminov <anton.linux@gmail.com>
2021-12-09 11:55:22 +0200

    day 9, part 1

---
+++ 2021/day09/part1.cr
@@ -0,0 +1,29 @@
+heightmap = File.read_lines("input").map(&.chars.map(&.to_i))
+
+rows = heightmap.size
+cols = heightmap.first.size
+
+low_points = [] of Int32
+
+heightmap.each_with_index do |locations, y|
+  locations.each_with_index do |number, x|
+    adjacent_locations = [] of Int32
+
+    left = right = up = down = nil
+
+    left = heightmap.dig(y, x - 1) if x > 0
+    right = heightmap.dig?(y, x + 1) if x < cols
+    down = heightmap.dig?(y - 1, x) if y > 0
+    up = heightmap.dig?(y + 1, x) if y < rows
+
+    adjacent_locations << left if left
+    adjacent_locations << right if right
+    adjacent_locations << down if down
+    adjacent_locations << up if up
+
+    low_points << number if adjacent_locations.all? { |n| number < n }
+  end
+end
+
+p low_points.map { |n| n += 1 }.sum
+# => 535
