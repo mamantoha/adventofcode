5396c1b5ba1c7246f3463e1ecd7a5bce0824c048
Anton Maminov <anton.linux@gmail.com>
2022-12-02 15:17:09 +0200

    use map().sum()

--- 2022/day02/part1.rs
+++ 2022/day02/part1.rs
@@ -9,36 +9,35 @@ fn main() {
     let draw = ["AX", "BY", "CZ"]; // 3
     let lose = ["AZ", "BX", "CY"]; // 0
 
-    let mut sum = 0;
+    let result: u32 = rounds
+        .iter()
+        .map(|round| {
+            let x1 = match round[1] {
+                "X" => 1,
+                "Y" => 2,
+                "Z" => 3,
+                &_ => 0,
+            };
 
-    rounds.iter().for_each(|round| {
-        // println!("{:?}", round[1]);
-        let x1 = match round[1] {
-            "X" => 1,
-            "Y" => 2,
-            "Z" => 3,
-            &_ => 0,
-        };
+            let combination = round.join("");
 
-        let combination = round.join("");
+            // https://doc.rust-lang.org/std/vec/struct.Vec.html#method.contains
 
-        // https://doc.rust-lang.org/std/vec/struct.Vec.html#method.contains
+            let x2 = if win.iter().any(|e| e == &combination) {
+                6
+            } else if draw.iter().any(|e| e == &combination) {
+                3
+            } else if lose.iter().any(|e| e == &combination) {
+                0
+            } else {
+                0
+            };
 
-        let x2 = if win.iter().any(|e| e == &combination) {
-            6
-        } else if draw.iter().any(|e| e == &combination) {
-            3
-        } else if lose.iter().any(|e| e == &combination) {
-            0
-        } else {
-            0
-        };
+            x1 + x2
+        })
+        .sum();
 
-        sum += x1 + x2;
-    });
-
-    // println!("{:?}", rounds);
-    println!("{:?}", sum);
+    println!("{:?}", result);
 }
 
 // 11841
