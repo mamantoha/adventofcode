9102380cb7652e4a12e19b1470c675cf834ccba1
Anton Maminov <anton.linux@gmail.com>
2022-12-02 16:26:17 +0200

    Update part2.rs

--- 2022/day02/part2.rs
+++ 2022/day02/part2.rs
@@ -13,54 +13,36 @@ fn main() {
     let draw = ["AX", "BY", "CZ"]; // 3
     let lose = ["AZ", "BX", "CY"]; // 0
 
-    let combination = "AZ";
-    println!("{:?}", lose.iter().find(|&&c| c == combination));
-
     let result: u32 = rounds
         .iter()
-        .map(|round| {
-            let x = match round[1] {
-                "X" => {
-                    let p = lose
-                        .iter()
-                        .find(|&&c| c.starts_with(round[0]))
-                        .unwrap()
-                        .chars()
-                        .last()
-                        .unwrap();
-
-                    points.get(&p).unwrap() + 0
-                }
-                "Y" => {
-                    let p = draw
-                        .iter()
-                        .find(|&&c| c.starts_with(round[0]))
-                        .unwrap()
-                        .chars()
-                        .last()
-                        .unwrap();
-
-                    points.get(&p).unwrap() + 3
-                }
-                "Z" => {
-                    let p = win
-                        .iter()
-                        .find(|&&c| c.starts_with(round[0]))
-                        .unwrap()
-                        .chars()
-                        .last()
-                        .unwrap();
-
-                    points.get(&p).unwrap() + 6
-                }
-                &_ => 0,
-            };
-
-            x
+        .map(|round| match round[1] {
+            "X" => {
+                let p = detect_point(round[0], lose.to_vec());
+                points.get(&p).unwrap() + 0
+            }
+            "Y" => {
+                let p = detect_point(round[0], draw.to_vec());
+                points.get(&p).unwrap() + 3
+            }
+            "Z" => {
+                let p = detect_point(round[0], win.to_vec());
+                points.get(&p).unwrap() + 6
+            }
+            &_ => 0,
         })
         .sum();
 
     println!("{:?}", result);
+}
+
+fn detect_point(x: &str, combinations: Vec<&str>) -> char {
+    combinations
+        .iter()
+        .find(|&&c| c.starts_with(x))
+        .unwrap()
+        .chars()
+        .last()
+        .unwrap()
 }
 
 // 13022
