67d17a690b2316512887945c6b0285de5be10a21
Anton Maminov <anton.linux@gmail.com>
2022-12-04 18:45:39 +0200

    Update part1-2.cr

--- 2022/day04/part1-2.cr
+++ 2022/day04/part1-2.cr
@@ -5,7 +5,7 @@ file_path = "input.txt"
 lines =
   File
     .read_lines(file_path)
-    .map(&.split(',').map(&.split('-').map(&.to_i)).map { |p| p[0]..p[1] })
+    .map(&.split(',').map(&.split('-').map(&.to_i)).map { |(b, e)| b..e })
 
 puts "Part 1:"
 puts lines.count { |p| p[0].covers?(p[1]) || p[1].covers?(p[0]) }
