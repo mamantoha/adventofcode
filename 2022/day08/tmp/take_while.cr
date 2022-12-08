b3b1aafb46f197395554a5239f221d6710e73921
Anton Maminov <anton.linux@gmail.com>
2022-12-08 21:04:06 +0200

    Update take_while.cr

--- 2022/day08/tmp/take_while.cr
+++ 2022/day08/tmp/take_while.cr
@@ -1,3 +1,13 @@
+# Passes elements to the block until the block returns a falsey value,
+# then stops iterating and returns an `Array` of all prior elements.
+#
+# Accepts an optional *include_falsey* parameter, which tells it to include
+# a falsey value to the result (defaults to `false`).
+#
+# ```
+# [1, 2, 3, 4, 5, 0].take_while { |i| i < 3 }       # => [1, 2]
+# [1, 2, 3, 4, 5, 0].take_while(true) { |i| i < 3 } # => [1, 2, 3]
+# ```
 module Enumerable(T)
   def take_while(include_falsey = false, & : T ->) : Array(T)
     result = Array(T).new
