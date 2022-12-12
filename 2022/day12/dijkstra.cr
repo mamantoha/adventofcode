a4411a22ed4b503771ed5ffeac85b0014ddd2575
Anton Maminov <anton.linux@gmail.com>
2022-12-12 17:16:50 +0200

    day 12: Dijkstra implementation is broken :-(

---
+++ 2022/day12/dijkstra.cr
@@ -0,0 +1,84 @@
+require "priority-queue"
+
+module Dijkstra
+  VERSION = {{ `shards version #{__DIR__}`.chomp.stringify }}
+
+  class Error < Exception
+  end
+
+  class Graph(T)
+    def initialize(@directed = false)
+      # {node1 => {edge1 => weight, edge2 => weight}, node2 => {...}, ...}
+      @neighbours = Hash(T, Hash(T, Int32)).new { |h, k| h[k] = Hash(T, Int32).new }
+
+      @vertices = Set(T).new
+      @infinity = Int32::MAX
+
+      @dist = {} of T => Int32
+      @prev = {} of T => T?
+    end
+
+    def add_edge(source : T, target : T, weight : Int32)
+      @neighbours[source][target] = weight
+      @neighbours[target][source] = weight unless @directed
+
+      @vertices << source << target
+    end
+
+    # Dijkstra's algorithm using a priority queue
+    #
+    # Based of wikipedia's pseudocode: https://en.wikipedia.org/wiki/Dijkstra's_algorithm
+    private def dijkstra(source : T)
+      raise Dijkstra::Error.new("Missing source node: '#{source}'") unless @vertices.includes?(source)
+
+      q = Priority::Queue(T).new
+
+      @vertices.each do |v|
+        @dist[v] = @infinity # Unknown distance from source to v
+        @prev[v] = nil       # Predecessor of v
+
+        q.push(@dist[v], v)
+      end
+
+      @dist[source] = 0
+
+      while !q.empty?   # The main loop
+        u = q.pop.value # Remove and return best vertex
+
+        next if @dist[u] == @infinity
+
+        @neighbours[u].each do |v, weight| # Go through all v neighbors of u
+          alt = @dist[u] + weight
+
+          if alt < @dist[v]
+            @dist[v] = alt
+            @prev[v] = u
+
+            q.push(alt, v) # decrease priority
+          end
+        end
+      end
+    end
+
+    def shortest_path(source : T, target : T) : {Int32, Array(T)}?
+      dijkstra(source)
+
+      raise Dijkstra::Error.new("Missing target node: '#{target}'") unless @vertices.includes?(target)
+
+      if @dist[target] != @infinity
+        {@dist[target], path_to(target)}
+      else
+        nil
+      end
+    end
+
+    # Full shortest route to a node
+    private def path_to(target, path = [] of T) : Array(T)
+      if (node = @prev[target])
+        path_to(node, path)
+      end
+
+      path << target
+    end
+  end
+end
---
+++ 2022/day12/part1.cr
@@ -0,0 +1,58 @@
+require "./dijkstra"
+
+# grid = File.read_lines("input_example.txt").map(&.chars)
+grid = File.read_lines("input.txt").map(&.chars)
+
+start = {0, 0}
+finish = {0, 0}
+
+visited = [] of Tuple(Int32, Int32)
+
+gr = Dijkstra::Graph(Tuple(Int32, Int32)).new(directed: true)
+
+# Set start/finish
+grid.each_with_index do |row, y|
+  row.each_with_index do |col, x|
+    if grid[y][x] == 'S'
+      start = {y, x}
+
+      grid[y][x] = 'a'
+    end
+
+    if grid[y][x] == 'E'
+      finish = {y, x}
+
+      grid[y][x] = 'z'
+    end
+  end
+end
+
+grid.each_with_index do |row, y|
+  row.each_with_index do |col, x|
+    current_val = grid[y][x]
+
+    # neighborhoods
+    [
+      {y - 1, x}, # ↑
+      {y, x + 1}, # →
+      {y + 1, x}, # ↓
+      {y, x - 1}, # ←
+    ].each do |dy, dx|
+      next unless 0 <= dy < grid.size
+      next unless 0 <= dx < grid.first.size
+
+      next_val = grid[dy][dx]
+
+      if current_val == next_val || current_val.ord == next_val.ord - 1
+        gr.add_edge({y, x}, {dy, dx}, 1)
+      end
+    end
+  end
+end
+
+p! start
+p! finish
+
+if (result = gr.shortest_path(start, finish))
+  p! result
+end
