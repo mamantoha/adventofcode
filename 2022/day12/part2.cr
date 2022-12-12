b02d672293e40ac8b44d366fc4bf0ab6838a454d
Anton Maminov <anton.linux@gmail.com>
2022-12-12 20:23:50 +0200

    day 12 part 2

---
+++ 2022/day12/part2.cr
@@ -0,0 +1,68 @@
+require "dijkstra"
+
+# grid = File.read_lines("input_example.txt").map(&.chars)
+grid = File.read_lines("input.txt").map(&.chars)
+
+starts = [] of Tuple(Int32, Int32)
+
+finish = {0, 0}
+
+visited = [] of Tuple(Int32, Int32)
+
+gr = Dijkstra::Graph(Tuple(Int32, Int32)).new(directed: true)
+
+# Set start/finish
+grid.each_with_index do |row, y|
+  row.each_with_index do |col, x|
+    if grid[y][x] == 'S'
+      grid[y][x] = 'a'
+    end
+
+    if grid[y][x] == 'E'
+      finish = {y, x}
+
+      grid[y][x] = 'z'
+    end
+
+    if grid[y][x] == 'a'
+      starts << {y, x}
+    end
+  end
+end
+
+grid.each_with_index do |row, y|
+  row.each_with_index do |col, x|
+    current_val = grid[y][x]
+
+    # neighborhoods
+    [
+      {y - 1, x}, # ↑
+      {y, x + 1}, # →
+      {y + 1, x}, # ↓
+      {y, x - 1}, # ←
+    ].each do |dy, dx|
+      next unless 0 <= dy < grid.size
+      next unless 0 <= dx < grid.first.size
+
+      next_val = grid[dy][dx]
+
+      # z -> y
+      if current_val == next_val || current_val.ord == next_val.ord + 1 || current_val.ord < next_val.ord
+        gr.add_edge({y, x}, {dy, dx}, 1)
+      end
+    end
+  end
+end
+
+results = [] of Int32
+
+starts.each do |start|
+  if (result = gr.shortest_path(finish, start))
+    if res = result.first
+      results << res
+    end
+  end
+end
+
+p! results.min
+# 478
