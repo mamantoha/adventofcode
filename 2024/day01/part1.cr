d4d725c4e7b19191c9c16ebf1bd5090120075b80
Anton Maminov <anton.linux@gmail.com>
2024-12-02 12:43:11 +0200

    2024 day 1

---
+++ 2024/day01/part1.cr
@@ -0,0 +1,11 @@
+puts File
+  .read_lines("input.txt")
+  .map(&.split(/\s+/)
+  .map(&.to_i))
+  .transpose
+  .map(&.sort)
+  .transpose
+  .map { |x| (x[0] - x[1]).abs }
+  .sum
+
+# 1660292
---
+++ 2024/day01/part2.cr
@@ -0,0 +1,9 @@
+left_list, right_list = File
+  .read_lines("input.txt")
+  .map(&.split(/\s+/)
+  .map(&.to_i))
+  .transpose
+
+puts left_list.reduce(0) { |acc, x| acc + (x * right_list.count(x)) }
+
+# 22776016
